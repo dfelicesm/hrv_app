d089687474db0a0f060fa7c39524bd73eba6ed60
Diego Felices Manero <dfelicesm@gmail.com>
2020-09-23 14:36:44 +0200

    Add data folder Load data into app

--- app.R
+++ app.R
@@ -3,11 +3,13 @@ library(shiny)
 ui <- fluidPage(
   selectInput(inputId = "basal_hrv_period", 
               label = "Length of basal HRV period:",
-              c("30 days", "60 days"))
+              c("30 days", "60 days")),
+  plotOutput("hrv_plot")
   
 )
 
 server <- function(input, output) {
+  output$hrv_plot <- renderPlot(hist(rnorm(100)))
   
 }
 
